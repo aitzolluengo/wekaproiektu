bb7eac358180edee5db89b943c741598d0e43edb
oiher <oiher.rodriguez@gmail.com>
2025-03-23 21:13:59 +0100

    azken bertsioa ONDO

--- src/Sailkatzailea/AtributuenAukeraketa.java
+++ src/Sailkatzailea/AtributuenAukeraketa.java
@@ -9,33 +9,23 @@ import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
-//import weka.attributeSelection.AttributeSelection;
 import weka.filters.supervised.attribute.AttributeSelection;
 
-
-// TODO: Auto-generated Javadoc
-/**
- * The Class AtributuenAukeraketa.
- */
 public class AtributuenAukeraketa {
 
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     * @throws Exception the exception
-     */
-    public static void main(String[] args) throws Exception{
-        if (args.length==3 || args.length==4) {
-            DataSource sauce = new DataSource(args[0]); // train data emen sartu
-            Instances data = sauce.getDataSet();
-            data.setClassIndex(data.numAttributes()-1);
+    public static void main(String[] args) throws Exception {
+        if (args.length == 3 || args.length == 4) {
+            // Cargar datos
+            DataSource source = new DataSource(args[0]);
+            Instances data = source.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1);
 
-
-            GainRatioAttributeEval gr = new GainRatioAttributeEval();	//InfoGainRatio atributu ebaluatzailea
-
-            Ranker ranker = new Ranker();		//atributuak rankeatzeko
-            if (args.length==4 && Integer.parseInt(args[3])<=300) ranker.setNumToSelect(Integer.parseInt(args[3]));			//atributu kopurua limitatzeko
+            // Aplicar selección de atributos
+            GainRatioAttributeEval gr = new GainRatioAttributeEval();
+            Ranker ranker = new Ranker();
+            if (args.length == 4 && Integer.parseInt(args[3]) <= 300) {
+                ranker.setNumToSelect(Integer.parseInt(args[3]));
+            }
             ranker.setThreshold(0.1);
 
             AttributeSelection as = new AttributeSelection();
@@ -44,25 +34,28 @@ public class AtributuenAukeraketa {
             as.setSearch(ranker);
             Instances filteredData = Filter.useFilter(data, as);
 
+            // Guardar el conjunto de datos filtrado
             ArffSaver saver = new ArffSaver();
             saver.setInstances(filteredData);
             saver.setFile(new File(args[1]));
             saver.writeBatch();
 
-            PrintWriter pw = new PrintWriter(args[2]);				//hiztegia gordetzeko
+            // Guardar la lista de atributos seleccionados
+            PrintWriter pw = new PrintWriter(args[2]);
             pw.println();
-            for (int i=0; i<filteredData.numAttributes()-1; i++) {
+            for (int i = 0; i < filteredData.numAttributes() - 1; i++) {
                 String s = filteredData.attribute(i).name();
-                pw.println(s.substring(0, s.length() - 2));
+                // Verificar que el nombre del atributo tenga al menos 2 caracteres
+                if (s.length() >= 2) {
+                    pw.println(s.substring(0, s.length() - 2));
+                } else {
+                    pw.println(s); // Si es demasiado corto, imprimir el nombre completo
+                }
             }
             pw.close();
-
+        } else {
+            System.out.println("3 parametro behar dira eta zuk " + args.length + " jarri dituzu!!");
+            System.out.println("java -jar AtributuenAukeraketa.jar trainPath.arff filteredPath.arff hiztegia atributuLimitea(aukerazkoa)");
         }
-        else {
-            System.out.println("3 parametro behar dira eta zuk "+args.length+" jarri dituzu!!");
-            System.out.println("java -jar FSS.jar trainPath.arff filteredPath.arff hiztegia atributuLimitea(aukerazkoa)");
-        }
-
     }
-
 }
